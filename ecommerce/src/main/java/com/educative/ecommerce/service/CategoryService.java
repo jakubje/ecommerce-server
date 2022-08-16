4a17bd61b63d337afa0c05456cb98d03aa502745
jakubje <jakubjeczalik@gmail.com>
2022-08-17 00:29:36 +0100

    Initial commit

---
+++ ecommerce/src/main/java/com/educative/ecommerce/service/CategoryService.java
@@ -0,0 +1,39 @@
+package com.educative.ecommerce.service;
+
+import com.educative.ecommerce.model.Category;
+import com.educative.ecommerce.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public Category readCategory(String categoryName) {
+        return categoryRepository.findByCategoryName(categoryName);
+    }
+    public Optional<Category> readCategory(Integer categoryId) {
+        return categoryRepository.findById(categoryId);
+    }
+
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public List<Category> listCategories(){
+        return categoryRepository.findAll();
+    }
+
+    public void updateCategory(Integer categoryID, Category newCategory) {
+        Category category = categoryRepository.findById(categoryID).get();
+        category.setCategoryName(newCategory.getCategoryName());
+        category.setDescription(newCategory.getDescription());
+        category.setImageUrl(newCategory.getImageUrl());
+        categoryRepository.save(category);
+    }
+}
